7a6e12ac452da2eceb8e8e8c4178b4d5119fe708
Josh Borrow <josh.borrow@gmail.com>
2017-03-27 20:13:56 +0100

    added first version of the flowchart

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 pdflatex -output-directory=compiled main.tex
 
 inkscape -D --file=plotgen/structure.svg --export-pdf=plotgen/structure.pdf
+inkscape -D --file=plotgen/flowchart.svg --export-pdf=plotgen/flowchart.pdf
 
 evince compiled/main.pdf &
