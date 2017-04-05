40369d2b32d0299f3beb31cd6b2db4d92573349f
Josh Borrow <josh.borrow@gmail.com>
2017-04-05 10:14:29 +0100

    added possibility to compile without remaking figures

--- compile.sh
+++ compile.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+
+echo "Generating Figures, please wait..."
+if [[ "$1" == "--figs" ]]; then
+    inkscape -D --file=plotgen/structure.svg --export-pdf=plotgen/structure.pdf
+    inkscape -D --file=plotgen/flowchart.svg --export-pdf=plotgen/flowchart.pdf
+
+    python3 plotgen/toomre_q.py
+fi
+
+echo " --##--##--##--##--##--##--##--##--##--##--##--##--##--##--##-- "
+echo "Running LaTeX"
 pdflatex -output-directory=compiled main.tex
 bibtex compiled/main.aux
 pdflatex -output-directory=compiled main.tex
 
-inkscape -D --file=plotgen/structure.svg --export-pdf=plotgen/structure.pdf
-inkscape -D --file=plotgen/flowchart.svg --export-pdf=plotgen/flowchart.pdf
 
 evince compiled/main.pdf &
