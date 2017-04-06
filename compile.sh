ab7837ec23081e937d6e8c219faad20b2ad977a8
Josh Borrow <josh.borrow@gmail.com>
2017-04-06 10:53:59 +0100

    minor fix

--- compile.sh
+++ compile.sh
@@ -4,9 +4,11 @@ echo "Generating Figures, please wait..."
 if [[ "$1" == "--figs" ]]; then
     inkscape -D --file=plotgen/structure.svg --export-pdf=plotgen/structure.pdf
     inkscape -D --file=plotgen/flowchart.svg --export-pdf=plotgen/flowchart.pdf
+    inkscape -z --file=plotgen/bigiel_extract.svg --export-png=plotgen/bigiel_extract.png --export-dpi=600
 
     python3 plotgen/toomre_q.py
     python3 plotgen/Q_analysis.py
+    python3 plotgen/bigiel_overplot.py
 fi
 
 echo " --##--##--##--##--##--##--##--##--##--##--##--##--##--##--##-- "
