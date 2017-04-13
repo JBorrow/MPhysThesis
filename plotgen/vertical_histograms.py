ee5065f05fec6b2249da3a34d7f273240f7f7a17
Josh Borrow <joshua.borrow@durham.ac.uk>
2017-04-13 10:03:06 +0100

    added exclusion of central region"

--- plotgen/vertical_histograms.py
+++ plotgen/vertical_histograms.py
@@ -24,7 +24,7 @@ names = ['custom', 'custom_highres', 'custom_lowres']
 
 
 class Vertical(object):
-    def __init__(self, filename, res, bbox_x, bbox_y, max_rad=30, n_bins=5, sf=1):
+    def __init__(self, filename, res, bbox_x, bbox_y, max_rad=30, n_bins=5, sf=1, exclude=0):
 
         data = survis.preprocess.DataGridder(filename,
                                              res[0],
@@ -43,8 +43,20 @@ class Vertical(object):
         self.n_bins = n_bins
         self.sf = sf  #scale factor
 
-        self.z_data = data.gas['Coordinates'][:, 2]
-        self.z_data_stars = data.star['Coordinates'][:, 2]
+        rads = np.sqrt(np.sum(np.square(data.gas['Coordinates']), 1))
+        rads_star = np.sqrt(np.sum(np.square(data.star['Coordinates']), 1))
+
+        mask = (rads < exclude)
+        mask_star = (rads_star < exclude)
+
+        z_data = data.gas['Coordinates'][:, 2]
+        z_data_stars = data.star['Coordinates'][:, 2]
+
+        mx = np.ma.masked_array(z_data, mask)
+        mx_star = np.ma.masked_array(z_data_stars, mask_star)
+
+        self.z_data = mx.compressed()
+        self.z_data_stars = mx_star.compressed()
 
         self.popt, self.pcov = self.get_scale_height(self.z_data, nbins=1000)
         self.popt_star, self.pcov_star = self.get_scale_height(self.z_data_stars, nbins=1000)
@@ -79,11 +91,11 @@ class Vertical(object):
         x_data = np.arange(min, max, (max - min)/nbins)
 
         for z, popt in zip([self.z_data, self.z_data_stars], [self.popt, self.popt_star]):
-            ax.hist(z, bins=int(1000/self.sf), range=(min, max), alpha=0.5)
+            ax.hist(z, bins=int(1000/self.sf), range=(min, max), alpha=0.5, histtype='stepfilled')
             y_data = self.to_fit(x_data, popt[0], popt[1], popt[2])
             ax.plot(x_data, y_data)
 
-        ax.text(min+0.02, namemax-150, "$Z_g = {:2.1f}$ pc\n$Z_* = {:3.0f}$ pc\n{}".format(1000*self.popt[1], abs(self.popt_star[1]*1000), name), fontsize=8)
+        ax.text(min+0.02, namemax, "$Z_g = {:2.1f}$ pc\n$Z_* = {:3.0f}$ pc\n{}".format(1000*abs(self.popt[1]), abs(self.popt_star[1]*1000), name), fontsize=8)
 
         ax.set_xlim(min, max)
 
@@ -96,6 +108,7 @@ if __name__ == "__main__":
     from matplotlib.ticker import MaxNLocator
     # Run in scripting mode, and actually perform the analysis
     output = {}
+    output_excl = {}  # Has the particles that are more than 0.5kpc out of the center.
 
     for simulation in tqdm(names):
         filename = "{}/snapshot_{:03d}.hdf5".format(simulation, snapshot)
@@ -109,12 +122,25 @@ if __name__ == "__main__":
     
         output[simulation] = data
 
+
+    for simulation in tqdm(names):
+        filename = "{}/snapshot_{:03d}.hdf5".format(simulation, snapshot)
+        tqdm.write("Attempting to analyse {} {}".format(simulation, snapshot))
+
+        if "lowres" in simulation:
+            data = Vertical(filename, res_lr, bbox_x, bbox_y, sf=10, exclude=0.5)
+        else:
+            data = Vertical(filename, res_nr, bbox_x, bbox_y, exclude=0.5)
+        tqdm.write("Analysis of {} {} successful.".format(simulation, snapshot))
+    
+        output_excl[simulation] = data
+
     
     # Now we do the postprocessing
 
-    f, axes = plt.subplots(1, 3, sharey=True, figsize=(6.3, 3)) 
+    f, axes = plt.subplots(2, 3, figsize=(6.3, 6)) 
 
-    for ax, simulation in zip(axes, output):
+    for ax, simulation in zip(axes[0, :], output):
         data = output[simulation]
 
         # Issue; plot overlaps with labels
@@ -123,12 +149,26 @@ if __name__ == "__main__":
         else:
             label = simulation
 
-        data.make_histogram(ax, name=label)
+        data.make_histogram(ax, name=label, namemax=660)
 
         ax.set_ylim(0, 800)
 
 
+    for ax, simulation in zip(axes[1, :], output_excl):
+        data = output_excl[simulation]
+
+        # Issue; plot overlaps with labels
+        if simulation != "custom":
+            label = simulation[7:]
+        else:
+            label = simulation
+
+        data.make_histogram(ax, name=label, namemax=85)
+
+        ax.set_ylim(0, 100)
+
     # Remove extra stuff
+    """
 
     nbins = len(axes[0].get_xticklabels())
     axes[0].xaxis.set_major_locator(MaxNLocator(nbins=nbins, prune='upper'))
@@ -137,7 +177,7 @@ if __name__ == "__main__":
 
     axes[1].set_xlabel("Height above midplane [kpc]")
     axes[0].set_ylabel("Number of particles in bin")
-
+"""
     plt.tight_layout()
     f.subplots_adjust(wspace=0)
 
